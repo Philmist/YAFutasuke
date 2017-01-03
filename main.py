d59c552f243239473ea56f5be4f7968026f83eff
philmist <philmist.j@gmail.com>
2017-01-03 22:20:15 +0900

    tornadoを使い始めるもいったんふりだしに戻る。

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
 #!/usr/bin/python3
 
-import tornado.ioloop
-import tornado.web
-from tornado.web import url
-import tornado.options
-from tornado.options import parse_command_line
+from futap2psv import ip_encrypt
 
-import signal
-import datetime
+import configparser
 
-from logging import getLogger
+from logging import getLogger, DEBUG
 
 logger = getLogger(__name__)
-
-
-def sig_handler(sig, frame):
-    """SIGINT,SIGTERMを受けとったときに処理する関数です。"""
-    logger.debug("SIGNAL: {}".format(sig))
-    tornado.ioloop.IOLoop.instance().add_callback(shutdown)
-
-
-def shutdown():
-    """全てを終了させる関数です。"""
-    logger.info("Shutdown...")
-    instance = tornado.ioloop.IOLoop.instance()
-    deadline = datetime.timedelta(seconds=30)
-
-    def terminate():
-        logger.info("Terminate.")
-        instance.stop()
-
-    instance.add_timeout(deadline, terminate)
-
-
-class MainHandler(tornado.web.RequestHandler):
-
-    def get(self):
-        self.write("HELLO WORLD!")
-
-
-def make_app():
-    return tornado.web.Application([
-        url(r"/", MainHandler),
-    ])
+logger.setLevel(DEBUG)
 
 
 if __name__ == "__main__":
-    parse_command_line()
-    app = make_app()
-    app.listen(8888)
-    ioloop = tornado.ioloop.IOLoop.current()
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
-    ioloop.start()
+    config = configparser.ConfigParser()
+    config.read(r"config.ini")
+    initial_ip = ip_encrypt.parse_disp_p2pnode(config["P2P"]["InitialNode"])[1]
+    inbound_ipport = config["P2P"]["InboundPort"]
+    inbound_ipaddr = config["P2P"]["InboundAddress"]
